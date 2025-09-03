4854ffb67413e6972f213d00489035b1a0961cd0
dom <domportera@gmail.com>
2025-09-03 19:19:03 -0400

    improve textbox......

--- Runtime/Scripts/Controls/Utility/ImDrawingUtility.cs
+++ Runtime/Scripts/Controls/Utility/ImDrawingUtility.cs
@@ -23,61 +23,86 @@ namespace Imui.Controls
             gui.Canvas.RectWithOutline(rect, style.BackColor, style.FrontColor, style.BorderThickness, style.BorderRadius);
         }
 
-        public static uint TextBox(this ImGui gui, ReadOnlySpan<char> text, ImTextSettings? textSettings = null, ImRect? rect = null, bool pushLayout = true)
+        public static void TextBox(this ImGui gui, ReadOnlySpan<char> text, ImTextSettings? textSettings = null, ImRect? rect = null, bool pushLayout = true, float inset = 0f, bool centerHorizontally = false)
         {
-            if (textSettings == null)
-            {
-                var settings = ImText.GetTextSettings(gui, true, ImTextOverflow.Truncate);
-                settings.Align = new ImAlignment(0.5f, 0.5f);
-                textSettings = settings;
-            }
-            
-            if (rect == null)
-            {
-                // measure the text
-                var maxWidth = gui.Layout.GetAvailableWidth();
-                var availableHeight = gui.Layout.GetAvailableHeight();
-                var maxHeight = availableHeight;
+            textSettings ??= GetDefaultCenteredTextSettings(gui);
+            var thisRect = rect ?? GetTextRectFromLayout(gui, text, textSettings, inset);
 
-                var extraSpacing= gui.Style.Layout.InnerSpacing * 2; 
-                if (maxHeight < gui.GetTextLineHeight() + extraSpacing)
+            if (centerHorizontally)
+            {
+                var availableWidth = gui.Layout.GetAvailableWidth();
+                var remainingWidth = availableWidth - thisRect.W;
+                if (remainingWidth > 0)
                 {
-                    maxHeight = 0;
+                    thisRect.X += remainingWidth * 0.5f;
                 }
-                
-                var measured = gui.MeasureTextSize(text, textSettings.Value, bounds: new Vector2(maxWidth, maxHeight));
-                rect = gui.AddLayoutRectWithSpacing(new Vector2(measured.x + extraSpacing, measured.y + extraSpacing));
             }
 
             var id = gui.GetNextControlId();
+            
             if (pushLayout)
             {
-                gui.Layout.Push(gui.Layout.Axis, rect.Value);
-            }
-            gui.RegisterControl(id, rect.Value);
-            gui.Box(rect.Value, gui.Style.Window.Box);
-            gui.Text(text, textSettings.Value, rect.Value);
-
-            if (pushLayout)
-            {
-                gui.Layout.Pop();
+                gui.Layout.AddRect(thisRect);
             }
             
-            return id;
+            gui.RegisterControl(id, thisRect);
+            gui.Box(thisRect, gui.Style.Window.Box);
+
+            var textRect = thisRect;
+            textRect.X += inset;
+            textRect.Y += inset;
+            var doubleInset = inset * 2;
+            textRect.W -= doubleInset;
+            textRect.H -= doubleInset;
+            gui.Text(text, textSettings.Value, textRect);
+        }
+
+        private static ImTextSettings GetDefaultCenteredTextSettings(ImGui gui)
+        {
+            var settings = ImText.GetTextSettings(gui, true, ImTextOverflow.Truncate);
+            settings.Align = new ImAlignment(0.5f, 0.5f);
+            return settings;
+        }
+
+        public static ImRect GetTextRectFromLayout(this ImGui gui, ReadOnlySpan<char> text, ImTextSettings? textSettings = null, float inset = 0f)
+        {
+            // measure the text
+            var spacing = gui.Style.Layout.InnerSpacing + inset;
+            var totalSpacing = spacing * 2;
+            var availableWidth = gui.Layout.GetAvailableWidth();
+            var availableHeight = gui.Layout.GetAvailableHeight();
+            var maxWidth = availableWidth - totalSpacing;
+            var maxHeight = availableHeight - totalSpacing;
+
+            if (maxHeight < gui.GetTextLineHeight())
+            {
+                maxHeight = 0;
+            }
+                
+            textSettings ??= GetDefaultCenteredTextSettings(gui);
+            var measured = gui.MeasureTextSize(text, textSettings.Value, bounds: new Vector2(maxWidth, maxHeight));
+            
+            var size = new Vector2(measured.x + totalSpacing, measured.y + totalSpacing);
+            
+            var bottomLeft = gui.Layout.GetContentRect().BottomLeft;
+            var rect = new ImRect(new(bottomLeft.x, bottomLeft.y - size.y), size);
+            return rect;
         }
 
         public static bool LoadingBar(this ImGui gui, float progress, ReadOnlySpan<char> label, Color? low = null, Color? high = null, ImStyleBox? style = null, float maxWidth = float.MaxValue, ImRect? rect = null)
         {
             style ??= gui.Style.Window.Box;
-            progress = progress < 0f ? 0f : progress > 1f ? 1f : progress;
             var bgStyle = style.Value;
             var fgStyle = bgStyle;
+            fgStyle.BackColor = Color.Lerp(low ?? Color.red, high ?? Color.green, progress);
+            fgStyle.BorderThickness = 0f;
+            
+            progress = progress < 0f ? 0f : progress > 1f ? 1f : progress;
             rect ??= gui.Layout.AddRect(MathF.Min(gui.Layout.GetAvailableWidth(), maxWidth), gui.GetRowHeight() + gui.Style.Layout.InnerSpacing * 2);//new ImSize(maxWidth, 0, ImSizeMode.Fill)));
             var progressRect = rect.Value;
             progressRect.W *= progress;
-                            
-            fgStyle.BackColor = Color.Lerp(low ?? Color.red, high ?? Color.green, progress);
             gui.RegisterControl(gui.GetNextControlId(), rect.Value);
+                            
             gui.Box(rect.Value, bgStyle);
             gui.Box(progressRect, fgStyle);
             
@@ -87,8 +112,8 @@ namespace Imui.Controls
 
             return progress >= 1f;
         }
-        
-        public static bool IsHovered(this ImGui gui, ImRect rect)
+
+        public static bool IsGuiElementHovered(this ImGui gui, ImRect rect)
         {
             ref readonly var meshProperties = ref gui.Canvas.GetActiveSettings();
             
